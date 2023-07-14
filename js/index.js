8df3d36f0516acf1cc4149bc5c8896a751500631
Ajmain Nishu <ajmainnishu@gmail.com>
2023-07-14 20:10:15 +0600

    feat: Meal details section added

--- js/index.js
+++ js/index.js
@@ -13,8 +13,8 @@ const loadUserFetch = async () => {
 
 const displayMealsCard = meals => {
     const mealsContainer = document.getElementById('meals-container');
+    mealsContainer.textContent = ``;
     meals.forEach(meal => {
-        // console.log(meal)
         const mealDiv = document.createElement('div');
         mealDiv.classList.add('col');
         mealDiv.innerHTML = 
@@ -33,9 +33,36 @@ const displayMealsCard = meals => {
 }
 
 
+const loadIdFetch = async (id) => {
+    try {
+        const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        displayMealDetails(data.meals[0]);
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
 
 
-
+const displayMealDetails = meal => {
+    console.log(meal);
+    const mealDetails = document.getElementById('meal-details');
+    mealDetails.textContent = ``;
+    const mealDiv = document.createElement('div');
+    mealDiv.classList.add('card');
+    mealDiv.innerHTML = 
+    `
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    `
+    mealDetails.appendChild(mealDiv);
+}
 
 
 
